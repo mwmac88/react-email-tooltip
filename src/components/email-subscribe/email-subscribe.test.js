ab337267d42ee9c2c2c006803c52a09570eab960
Matt Maclean <mwmaclean88@gmail.com>
2019-08-25 17:19:25 +0100

    feat: add basic jest enzyme test

--- src/components/email-subscribe/email-subscribe.test.js
+++ src/components/email-subscribe/email-subscribe.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import EmailSubscribe from './index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<EmailSubscribe />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('EmailSubscribe Component', () => {
+  let wrapper;
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, 'useState')
+  useStateSpy.mockImplementation((init) => [init, setState]);
+
+  beforeEach(() => {
+    wrapper = shallow(<EmailSubscribe visible={false} />);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders without crashing', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('has default visible prop', () => {
+    expect(wrapper.props().visible).toBe(false);
+  });
+})
+
+
+
