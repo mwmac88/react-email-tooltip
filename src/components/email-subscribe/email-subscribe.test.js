98460d46e4cdcb5c04824b1407672ea0918986bc
Matt Maclean <mwmaclean88@gmail.com>
2019-08-26 10:09:50 +0100

    feat: add additional basic tests to emailsubscribe

--- src/components/email-subscribe/email-subscribe.test.js
+++ src/components/email-subscribe/email-subscribe.test.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 import EmailSubscribe from './index';
 
 describe('EmailSubscribe Component', () => {
   let wrapper;
+  let styledEmailSubscribe;
   const setState = jest.fn();
   const useStateSpy = jest.spyOn(React, 'useState')
   useStateSpy.mockImplementation((init) => [init, setState]);
 
   beforeEach(() => {
     wrapper = shallow(<EmailSubscribe visible={false} />);
+    styledEmailSubscribe = renderer.create(<EmailSubscribe visibile={false} />);
   });
 
   afterEach(() => {
@@ -21,10 +23,30 @@ describe('EmailSubscribe Component', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('has default visible prop', () => {
+  it('has default props as false on initial load', () => {
     expect(wrapper.props().visible).toBe(false);
   });
-})
 
+  it('does not display on initial load', () => {
+    const styledEmailSubscribeJSON = styledEmailSubscribe.toJSON();
+    expect(styledEmailSubscribeJSON).toHaveStyleRule('display', 'none');
+    expect(styledEmailSubscribeJSON).toMatchSnapshot();
+  })
 
+  describe('Show EmailSubscribe component', () => {
+    beforeEach(() => {
+      wrapper = shallow(<EmailSubscribe visible={true} />);
+      styledEmailSubscribe = renderer.create(<EmailSubscribe visible={true} />);
+    });
 
+    it('has visible prop set as true', () => {
+      expect(wrapper.props().visible).toBe(true);
+    });
+
+    it('displays the component successfully', () => {
+      const styledEmailSubscribeJSON = styledEmailSubscribe.toJSON();
+      expect(styledEmailSubscribeJSON).toHaveStyleRule('display', 'block');
+      expect(styledEmailSubscribeJSON).toMatchSnapshot();
+    })
+  });
+});
